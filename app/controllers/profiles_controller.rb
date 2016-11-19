c8cb552b448fbd7c361af3fe6232ef9ba193af03
Josh Terry <joshterry55@gmail.com>
2016-11-19 10:36:48 -0700

    profiles controller crud action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,53 @@
 class ProfilesController < ApplicationController
+  before_action :set_profile, except: [:index, :new, :create]
+
   def index
+    @profiles = Profiles.all
   end
 
   def show
   end
 
   def new
+    @profile = current_user.profiles.new
+  end
+
+  def create
+    @profile = current_user.profiles.new(profile_params)
+    if @profile.save
+      flash[:success] = 'Profile Created Successfully!'
+      redirect_to profile_path(@profile)
+    else
+      flash[:error] = 'Something Went Wrong!'
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @profile.update(profile_params)
+      flash[:success] = 'Profile Updated Successfully!'
+      redirect_to profile_path(@profile)
+    else
+      flash[:error] = 'Something Went Wrong!'
+      render :edit
+    end
+  end
+
+  def destroy
+    @profile.destroy
+    flash[:success] = 'Profile Deleted Successfully!'
+    redirect_to profiles_path
+  end
+
+  private
+  def set_profile
+    @profile = current_user.profiles.find(params[:id])
+  end
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :email, :language, :experience, :bio)
   end
 end
