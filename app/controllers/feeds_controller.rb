eaf8c2924a31fa9eb0e9930a979406addf1d9f5d
Robert Bell <robell7837@hotmail.com>
2016-11-19 10:39:13 -0700

    feeds controller working

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,11 +1,15 @@
 class FeedsController < ApplicationController
 
-
   def index
+    @feed = Feed.all
   end
 
   def show
+    @feed = Feed.category(feed_params)
   end
 
-
+  private
+    def feed_params
+      params.require(:feed).permit(:category)
+    end
 end
